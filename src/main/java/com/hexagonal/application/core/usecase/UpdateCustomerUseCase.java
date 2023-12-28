a14f372ec485e4015a6a1688babca6d47b0d5f92
Barbara Pinheiro <bcmpinheiro@gmail.com>
2023-12-28 00:06:41 -0300

    feat: criando usecase para o update

---
+++ src/main/java/com/hexagonal/application/core/usecase/UpdateCustomerUseCase.java
@@ -0,0 +1,32 @@
+package com.hexagonal.application.core.usecase;
+
+import com.hexagonal.application.core.domain.Customer;
+import com.hexagonal.application.ports.in.FindCustomerByIdInputPort;
+import com.hexagonal.application.ports.out.FindAddressByZipCodeOutputPort;
+import com.hexagonal.application.ports.out.UpdateCustomerOutputPort;
+
+public class UpdateCustomerUseCase {
+
+    private final FindCustomerByIdInputPort findCustomerByIdInputPort;
+
+    private final FindAddressByZipCodeOutputPort findAddressByZipCodeOutputPort;
+
+    private final UpdateCustomerOutputPort updateCustomerOutputPort;
+
+    public UpdateCustomerUseCase(
+            FindCustomerByIdInputPort findCustomerByIdInputPort,
+            FindAddressByZipCodeOutputPort findAddressByZipCodeOutputPort,
+            UpdateCustomerOutputPort updateCustomerOutputPort
+    ){
+        this.findCustomerByIdInputPort = findCustomerByIdInputPort;
+        this.findAddressByZipCodeOutputPort = findAddressByZipCodeOutputPort;
+        this.updateCustomerOutputPort = updateCustomerOutputPort;
+    }
+
+    public void update(Customer customer, String zipCode) {
+        findCustomerByIdInputPort.find(customer.getId());
+        var address = findAddressByZipCodeOutputPort.find(zipCode);
+        customer.setAddress(address);
+        updateCustomerOutputPort.update(customer);
+    }
+}
