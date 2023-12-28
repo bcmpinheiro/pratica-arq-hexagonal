43a9b13c55807a5b7fc6170f280e820c9a8fe95b
Barbara Pinheiro <bcmpinheiro@gmail.com>
2023-12-28 00:23:07 -0300

    feat: criando adapter para o delete

---
+++ src/main/java/com/hexagonal/adapters/out/DeleteCustomerByIdAdapter.java
@@ -0,0 +1,18 @@
+package com.hexagonal.adapters.out;
+
+import com.hexagonal.adapters.out.repository.CustomerRepository;
+import com.hexagonal.application.ports.out.DeleteCustomerByIdOutputPort;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DeleteCustomerByIdAdapter implements DeleteCustomerByIdOutputPort {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Override
+    public void delete(String id) {
+        customerRepository.deleteById(id);
+    }
+}
